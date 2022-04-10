d5d1d854299ad10e85fcf3ea92196a132586ae3d
cumet04 <medalhkr@gmail.com>
2022-04-10 07:27:15 +0000

    setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,6 +5,10 @@ cd $(readlink -f $0 | xargs dirname)/.. # workplace root
 
 echo 'install dev packages:'
 
+cd frontend
+echo '[frontend] npm ci'; npm ci
+cd ..
+
 cd backend
 echo '[backend] bundle'; bundle
 # echo '[backend] db:reset'; bundle exec rails db:reset
