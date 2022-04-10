e054398c769a1678430aadb3deba99bb4f890b18
cumet04 <medalhkr@gmail.com>
2022-04-10 12:40:32 +0000

    hello next

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,56 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 
-const Home: NextPage = () => {
-  return <div>hoge</div>;
+type EventData = {
+  title: string;
+  startAt: string;
+  location: string;
 };
 
+type Event = {
+  title: string;
+  startAt: Date;
+  location: string;
+};
+
+type Props = {
+  eventData: EventData[];
+};
+
+const Home: NextPage<Props> = (props) => {
+  const events: Event[] = props.eventData.map((d) => ({
+    title: d.title,
+    startAt: new Date(Date.parse(d.startAt)),
+    location: d.location,
+  }));
+
+  return (
+    <>
+      <ul>
+        {events.map((e) => (
+          <li key={e.title}>{e.title}</li>
+        ))}
+      </ul>
+      <style jsx>{`
+        li {
+          border-bottom: 1px solid black;
+        }
+      `}</style>
+    </>
+  );
+};
 export default Home;
+
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
+  const eventData: EventData[] = [
+    { title: "event1", startAt: "2022-04-10", location: "foo" },
+    { title: "event2", startAt: "2022-04-09", location: "bar" },
+  ];
+
+  return {
+    props: {
+      eventData,
+    },
+  };
+};
